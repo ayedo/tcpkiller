87fb2888dcfac2e66cfcd9357089267f2d353d1e
Alon Dolev <alon@dolev.ch>
2020-03-20 21:05:26 +0100

    adds gradle plugin to create a native installer for the manager

--- src/main/kotlin/ch/ayedo/portmanager/PortManager.kt
+++ src/main/kotlin/ch/ayedo/portmanager/PortManager.kt
@@ -4,18 +4,25 @@ import ch.ayedo.portmanager.views.MainView
 import javafx.scene.image.Image
 import javafx.stage.Stage
 import tornadofx.App
+import tornadofx.launch
 
 
 class PortManager : App(MainView::class) {
+
     override fun start(stage: Stage) {
         with(stage) {
             width = 800.0
             height = 600.0
             title = "Port Manager"
         }
-        
+
         stage.icons.add(Image(this::class.java.getResourceAsStream("/logo.png")));
 
         super.start(stage)
     }
+
+}
+
+fun main(args: Array<String>) {
+    launch<PortManager>(args)
 }
